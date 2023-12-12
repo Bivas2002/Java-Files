266986371772c646ab1859f636d5f84bfa560471
Bivas2002 <141204008+Bivas2002@users.noreply.github.com>
2023-12-13 02:25:40 +0600

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,12 @@
+public class Car {
+    String Name = "Tesla";
+    String Model = "675G";
+    int year =  2024;
+
+    void drive(){
+        System.out.println("The car is moving.");
+    }
+    void park(){
+        System.out.println("The car is parked!");
+    }
+}
